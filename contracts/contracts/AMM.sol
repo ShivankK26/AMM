8530c64b8e755bbf377fe8025b9fc3df85600b66
Shivank Kapur <shivankkapur2004@gmail.com>
2024-11-16 02:03:19 +0530

    feat: defined the mappings, modifiers for the contract.

---
+++ contracts/contracts/AMM.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >= 0.7.0 < 0.9.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract AMM {
+    using SafeMath for uint256;
+    uint256 totalShares;  // Stores the total amount of share issued for the pool
+    uint256 totalToken1;  // Stores the amount of Token1 locked in the pool
+    uint256 totalToken2;  // Stores the amount of Token2 locked in the pool
+    uint256 K;            // Algorithmic constant used to determine price
+
+    uint256 constant PRECISION = 1_000_000;  // Precision of 6 digits
+
+    mapping(address => uint256) shares;  // Stores the share holding of each provider
+
+    // Stores the available balance of user outside of the AMM
+    // For simplicity purpose, We are maintaining our own internal 
+    // balance mapping instead of dealing with ERC-20 tokens
+    mapping(address => uint256) token1Balance;
+    mapping(address => uint256) token2Balance;
+
+    // Ensures that the _qty is non-zero and the user has enough balance
+    modifier validAmountCheck(mapping(address => uint256) storage _balance, uint256 _qty) {
+        require(_qty > 0, "Amount cannot be zero!");
+        require(_qty <= _balance[msg.sender], "Insufficient amount");
+        _;
+    }
+    
+    // Restricts withdraw, swap feature till liquidity is added to the pool
+    modifier activePool() {
+        require(totalShares > 0, "Zero Liquidity");
+        _;
+    }
+}
